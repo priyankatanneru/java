1d407b52b6cca01360415edced73ad6f57fdcf17
priyankatanneru <tannerupriyanka712@gmail.com>
2026-06-03 07:13:13 +0000

    Added Java WaterBottle program

--- Main.java
+++ Main.java
@@ -6,13 +6,26 @@ class WaterBottle {
         System.out.println("Bottle is filled");
     }
 }
+//child class
+
+class Thermos extends WaterBottle {
+    void keepHot() {
+        System.out.println("Thermos keeps water hot");
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         WaterBottle bottle1 = new WaterBottle(); // Object
+        Thermos thermos1 = new Thermos(); // Object
+        thermos1.color = "Red";
         bottle1.color = "Blue";
         bottle1.capacity = 1000;
 
-        bottle1.fill();
+        bottle1.fill(); //inherited method
+        thermos1.keepHot(); // own method
+        thermos1.fill(); // inherited method
+        System.out.println("Bottle color: " + bottle1.color);
+        System.out.println("Thermos color: " + thermos1.color);
     }
 }
