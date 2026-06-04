4ffda8901e9b98c44d54134b3f786de8628021b3
priyankatanneru <tannerupriyanka712@gmail.com>
2026-06-04 05:29:40 +0000

    Added

--- Collections.java
+++ Collections.java
@@ -9,5 +9,21 @@ class Collections{
         list.add("Hello"); // ArrayList allows duplicates
 
         System.out.println("ArrayList: " + list);
+
+        //HashSet : A Set stores unique elements and does not allow duplicates.
+        HashSet<String> set = new HashSet<>();
+        set.add("Hello");
+        set.add("World");
+        set.add("Hello"); // HashSet does not allow duplicates
+
+        System.out.println("HashSet: " + set);
+
+        //HashMap : A Map stores key-value pairs and does not allow duplicate keys.
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("One", 1);
+        map.put("Two", 2);
+        map.put("One", 11); // HashMap does not allow duplicate keys, it will overwrite the value for key "One"
+
+        System.out.println("HashMap: " + map);
     }
 }
